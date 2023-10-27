f823d590f061fe68e9ae1a13cce3abd5caeecfe8
DmitryGorbunow <dmitry.gorbunow@gmail.com>
2023-10-27 16:36:48 +0500

    Initial Commit

---
+++ MVP+R-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP+R-Demo
+//
+//  Created by Dmitry Gorbunow on 10/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
